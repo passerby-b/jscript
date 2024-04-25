a14ceef84c1a736628bd0739e58161fafd3f7960
passerby-b <40885345+passerby-b@users.noreply.github.com>
2024-04-25 17:16:54 +0800

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 {
   "mtkey":"00006b12-1188-4cbe-9bf5-528fcf8fe4dc",
-  "dyplkey":"00006b12-1188-4cbe-9bf5-528fcf8fe4de"
+  "dyplkey":"00006b12-1188-4cbe-9bf5-528fcf8fe4de",
+  "jdkey":"2116d854-fc46-4763-84e9-774e0a6bc016"
 }
